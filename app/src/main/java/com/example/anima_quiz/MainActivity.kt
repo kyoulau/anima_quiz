b8b3ac2968c2ecc3704937ea8907874769f2059e
petersudario <pedrohesudario@hotmail.com>
2024-10-30 20:49:45 -0300

    UI folder organization

--- app/src/main/java/com/example/anima_quiz/MainActivity.kt
+++ app/src/main/java/com/example/anima_quiz/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.anima_quiz.ui.components.Greeting
+import com.example.anima_quiz.ui.screens.HomeScreen
 import com.example.anima_quiz.ui.theme.Anima_quizTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +18,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Anima_quizTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Anima_quizTheme {
-        Greeting("Android")
-    }
-}
+
---
+++ app/src/main/java/com/example/anima_quiz/ui/components/Greetings.kt
@@ -0,0 +1,23 @@
+package com.example.anima_quiz.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.anima_quiz.ui.theme.Anima_quizTheme
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Anima_quizTheme {
+        Greeting("Android")
+    }
+}
