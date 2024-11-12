659beb47ca06fc7c81924e4520b6e7716c20df69
andretini <andrealijaramosagostini@gmail.com>
2024-11-11 21:15:46 -0300

    estilizacao

--- app/src/main/java/com/example/anima_quiz/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/anima_quiz/ui/screens/MainScreen.kt
@@ -1,15 +1,23 @@
 package com.example.anima_quiz.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.input.TextFieldDecorator
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +25,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,13 +40,22 @@ fun MainScreen(onContinueClicked: (String) -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primary.copy(alpha = 0.4f))
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Insira seu nome para Continuar",
-            style = MaterialTheme.typography.titleSmall,
+            text = "Bem-vindo ao Quiz!",
+            style = MaterialTheme.typography.headlineSmall,
+            color = Color.White,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        Text(
+            text = "Insira seu nome para continuar:",
+            style = MaterialTheme.typography.bodyLarge,
+            color = Color.White,
             modifier = Modifier.semantics {
                 contentDescription = "Instrução para inserir o nome"
             }
@@ -44,10 +63,23 @@ fun MainScreen(onContinueClicked: (String) -> Unit) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
+
+        TextField(
             value = userName,
             onValueChange = { userName = it },
-            label = { Text("Nome") }
+            label = { Text("Nome") },
+            modifier = Modifier
+                .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
+                .clip(RoundedCornerShape(8.dp))
+                .background(Color.White)
+                .padding(4.dp),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                cursorColor = Color.Black
+            )
         )
 
         Spacer(modifier = Modifier.height(16.dp))
